686394d96bc6b2052d95f8c0ad5fe94203a7932d
Michael Williams <dinosaur@riseup.net>
2014-08-31 11:07:38 -0700

    remove rcss

--- example/app.js
+++ example/app.js
@@ -1,12 +1,9 @@
 var mercury = require('mercury');
-var RCSS = require('rcss');
 
 var GridUi = require('grid-ui');
 var PersonUi = require('person-ui');
 
 var DataUi = require('../');
-
-RCSS.injectAll()
 
 var people = [{
   id: "http://dinosaur.is#i",
